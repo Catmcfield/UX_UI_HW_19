995d703e099918497fe9a52e31584c35cb3f88bf
Catmcfield <125141663+Catmcfield@users.noreply.github.com>
2023-07-29 22:30:32 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,26 @@
 console.log ("Your index.js file is loaded correctly!");
-$("content").scroll(function(){
-  $("button").fadeToggle("slow");
-});
+
+$(document).ready(function(){
+  $('.content').on( "mouseenter", function(){
+    $("button").animate({'opacity':'1'},500);
+    });
+  });
+
+  $(document).ready(function(){
+    $('.Creaven').on( "mouseenter", function(){
+      $('.creaven-button').animate({'opacity':'1'},500);
+      });
+    });
+    $(document).ready(function(){
+      $('.Unicef').on( "mouseenter", function(){
+        $('.unicef-button').animate({'opacity':'1'},500);
+        });
+      });
+      $(document).ready(function(){
+        $('.DOL').on( "mouseenter", function(){
+          $('.dol-button').animate({'opacity':'1'},500);
+          });
+        });
+   
+
+
